2fad04b23d6d33346f804e128c0f27ff31ea771b
saumya-singh <saumya.singh0993@gmail.com>
2018-02-20 23:51:42 +0530

    defined it under function

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
 import socket
 
-HOST, PORT = '', 8000
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print('Serving HTTP on port %s ...' % PORT)
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print(request)
+def execute_server(HOST, PORT):
+    # HOST, PORT = '', 8000
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+    print('Serving HTTP on port %s ...' % PORT)
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        request = client_connection.recv(1024)
+        http_response = "\nHTTP/1.1 200 OK\n\nHello, World!\n"
+        client_connection.send(http_response.encode())
+        client_connection.close()
 
-    http_response = """\
-HTTP/1.1 200 OK
 
-Hello, World!
-"""
-    client_connection.sendall(http_response.encode())
-    client_connection.close()
+if __name__ == '__main__':
+    execute_server("localhost", 8000)
