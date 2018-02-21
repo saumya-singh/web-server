520c4477fcf592a707a30c5de6926b478a2af03b
saumya-singh <saumya.singh0993@gmail.com>
2018-02-21 13:50:53 +0530

    made changes in request dictionary and added method_handler

--- server.py
+++ server.py
@@ -1,42 +1,71 @@
 import socket, pprint
 
 
-# METHODS = {"GET" : get_handler,
-#     "POST" : post_handler,
-#     "PUT" : put_handler,
-#     "DELETE" : delete_handler
-# }
+def get_handler(request, response):
+    pass
 
 
-def header_parser(header_stream):
+def method_handler(request, response):
+    METHODS = {"GET" : get_handler,
+        "POST" : post_handler,
+        "PUT" : put_handler,
+        "DELETE" : delete_handler
+    }
+    handler = METHODS[request["method"]]
+    return handler(request, response)
+
+
+def request_handler(request):
+    response = {}
+    # response = "\nHTTP/1.1 200 OK\n\nHello, World!\n"
+    return method_handler(request, response)
+
+
+def get_query_content(header):
+    content = {}
+    path, query_params=  header["path"].split("?")
+    header["path"] = path
+    query_list = query_params.split("&")
+    for each_query in query_list:
+        key, val = each_query.split("=")
+        content[key] = val
+    return (header["path"], content)
+
+
+def header_parser(request, header_stream):
     header = {}
     header_list = header_stream.split(b"\r\n")
-    method, path, http_version = (header_list[0].decode()).split(" ")
-    header["method"] = method
-    header["path"] = path
-    header["http_version"] = http_version
-    no_of_headers = len(header_list)
-    for index in range(1, no_of_headers):
-        one_header = header_list[index].decode()
-        colon_pos = one_header.find(":")
-        header_name = one_header[ : colon_pos]
-        header[header_name] = one_header[colon_pos + 2 : ]
-    return header
+    request["method"], request["path"], request["http_version"] = \
+                                    (header_list[0].decode()).split(" ")
+    if "?" in request["path"]:
+        request["path"], request["content"] = get_query_content(header)
+    header_list.pop(0)
+    for one_header in header_list:
+        key, value = (one_header.decode()).split(": ")
+        header[key] = value
+    if "Cookie" in header:
+        cookie_content = {}
+        cookies = header["Cookie"].split(";")
+        for each_cookie in cookies:
+            key, val = each_cookie.split("=")
+            cookie_content[key] = val
+        header["Cookie"] = cookie_content
+    request["header"] = header
+    return request
 
 
 def request_parser(request_data):
+    request = {}
     header_stream = request_data.split(b"\r\n\r\n")[0]
-    header = header_parser(header_stream)
-    # pprint.pprint(header)
-    if "Content-Length" in header:
+    request = header_parser(request, header_stream)
+    if "Content-Length" in request["header"]:
         body_stream = request_data.split(b"\r\n\r\n")[1]
-        # print(body_stream)
-    response = "\nHTTP/1.1 200 OK\n\nHello, World!\n"
-    return response
+        request["body"] = body_stream.decode()
+    pprint.pprint(request) ##
+    return request
 
 
 def execute_server(HOST, PORT):
-    # HOST, PORT = '', 8000
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind((HOST, PORT))
@@ -45,8 +74,8 @@ def execute_server(HOST, PORT):
     while True:
         client_connection, client_address = listen_socket.accept()
         request_data = client_connection.recv(1024)
-        print(request_data)
-        http_response = request_parser(request_data)
+        request = request_parser(request_data)
+        http_response = request_handler(request)
         client_connection.send(http_response.encode())
         client_connection.close()
 
