327f46aba1caaaea87a1f4c75cbf2e9e13092ebb
saumya-singh <saumya.singh0993@gmail.com>
2018-02-22 08:49:02 +0530

    added function to form response

--- server.py
+++ server.py
@@ -1,15 +1,63 @@
-import socket, pprint
+import socket
+import pprint
+import datetime
+
+
+CONTENT_TYPE = {
+    "html": "text/html",
+    "css": "text/css",
+    "js": "application/javascript",
+    "jpeg": "image/jpeg",
+    "ico": "image/x-icon",
+    "text": "text/plain",
+    "json": "application/json"
+}
+
+
+def form_response(response):
+    res = b""
+    print(response["status_line"])
+    res += response["status_line"] + "\r\n"
+    # res += response["Content-Type"] + "\r\n"
+    # res += response["Content-Length"] + "\r\n\r\n"
+    # res += response["content"]
+    # print("====", res.encode() + response["content"])
+    return res.encode() + response["content"]
+
+
+def success_handler(request, response):
+    response["status_line"] = "HTTP/1.1 20 OK"
+    if response["content"]:
+        response["Content-Length"] = str(len(response["content"]))
+    response = form_response(response)
+    return response
+
+
+def error_handler(request, response):
+    pass
+
+
+def static_file_handler(request, response):
+    try:
+        with open("./static" + request["path"], "rb") as file_obj:
+            res_body = file_obj.read()
+    except OSError:
+        return error_handler(request, response)
+    response["content"] = res_body
+    content_type = request["path"].split(".")[1]
+    response["Content-Type"] = CONTENT_TYPE[content_type]
+    return success_handler(request, response)
 
 
 def get_handler(request, response):
-    pass
+    return static_file_handler(request, response)
 
 
 def method_handler(request, response):
     METHODS = {"GET" : get_handler,
-        "POST" : post_handler,
-        "PUT" : put_handler,
-        "DELETE" : delete_handler
+        # "POST" : post_handler,
+        # "PUT" : put_handler,
+        # "DELETE" : delete_handler
     }
     handler = METHODS[request["method"]]
     return handler(request, response)
@@ -76,7 +124,8 @@ def execute_server(HOST, PORT):
         request_data = client_connection.recv(1024)
         request = request_parser(request_data)
         http_response = request_handler(request)
-        client_connection.send(http_response.encode())
+        # print("++++++", http_response)
+        client_connection.send(http_response)
         client_connection.close()
 
 
