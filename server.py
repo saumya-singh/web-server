3f7879711416ef57e4cc53a407e72fd759d44ac2
saumya-singh <saumya.singh0993@gmail.com>
2018-03-13 16:18:08 +0530

    merged request_parser and handle_message functions

--- server.py
+++ server.py
@@ -84,6 +84,7 @@ def route_handler(request, response, next_):
 
 
 def static_file_handler(request, response, next_):
+    print("\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n")
     if request["method"] == "GET":
         try:
             if request["path"][-1] == "/":
@@ -140,14 +141,14 @@ def get_query_content(request):
 
 def header_parser(request, header_stream):
     header = {}
-    header_list = header_stream.split(b"\r\n")
+    header_list = header_stream.split("\r\n")
     request["method"], request["path"], request["http_version"] = \
-                                    (header_list[0].decode()).split(" ")
+                                    (header_list[0].split(" "))
     if "?" in request["path"]:
         request["path"], request["content"] = get_query_content(request)
     header_list.pop(0)
     for one_header in header_list:
-        key, value = (one_header.decode()).split(": ")
+        key, value = (one_header).split(": ")
         header[key] = value
     if "Cookie" in header:
         cookie_content = {}
@@ -157,49 +158,25 @@ def header_parser(request, header_stream):
             cookie_content[key] = val
         header["Cookie"] = cookie_content
     request["header"] = header
-
-
-def request_parser(request_data):
-    request = {}
-    header_stream = request_data.split(b"\r\n\r\n")[0]
-    header_parser(request, header_stream)
-    if "Content-Length" in request["header"]:
-        body_stream = request_data.split(b"\r\n\r\n")[1]
-        request["body"] = body_stream.decode()
     return request
 
 
-# def execute_server(HOST, PORT):
-#     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     listen_socket.bind((HOST, PORT))
-#     listen_socket.listen(1)
-#     print('Serving HTTP on port %s ...' % PORT)
-#     while True:
-#         client_connection, client_address = listen_socket.accept()
-#         request_data = client_connection.recv(1024)
-#         if request_data == b'':
-#             client_connection.close()
-#         print("*********")
-#         print(request_data)
-#         print("*********")
-#         request = request_parser(request_data)
-#         print("==================")
-#         pprint.pprint(request)
-#         print("==================")
-#         http_response = request_handler(request)
-#         print("response:\n", http_response)
-#         client_connection.send(http_response)
-#         client_connection.close()
-
-
 async def handle_message(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
     addr = writer.get_extra_info('peername')
-    print("Received %r from %r" % (message, addr))
-
-    writer.write(data)
+    # print("Received from %r" % (addr))
+    header = await reader.readuntil(b'\r\n\r\n')
+    header_stream = header.decode().split("\r\n\r\n")[0]
+    print("==========", header_stream, "===========")
+    request = {}
+    request = header_parser(request, header_stream)
+    if "Content-Length" in request["header"]:
+        con_len = request["header"]["Content-Length"]
+        body_stream = await reader.readexactly(con_len)
+        print("++++++++++", body_stream.decode(), "++++++++++++")
+        request["body"] = body_stream.decode()
+    pprint.pprint(request)
+    response = request_handler(request)
+    writer.write(response)
     await writer.drain()
     print("Close the client socket")
     writer.close()
@@ -220,4 +197,4 @@ def execute_server():
 
 
 if __name__ == '__main__':
-    execute_server("localhost", 8000)
+    execute_server()
