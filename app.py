1f4e82d4338a0a6b7a70b2e61fdfc8cd67604f6f
saumya-singh <saumya.singh0993@gmail.com>
2018-03-16 11:49:39 +0530

    modified the code for redirect

--- app.py
+++ app.py
@@ -4,28 +4,30 @@
 import json
 import server
 
+
 def render_form(request, response, id):
-    print("$$$$$$$$$$$$", id)
-    path = 'static/form.html'
-    with open(path, 'r') as file_obj:
-        res_content = file_obj.read()
-    server.res_status(response, 302)
-    # header = {}
-    # server.res_header(request, response, header)
-    print(response)
-    return res_content
+    return server.redirect(request, response, "http://www.example.com/", 307)
+    # path = 'static/form.html'
+    # with open(path, 'r') as file_obj:
+    #     res_content = file_obj.read()
+    # # server.res_status(response, 302)
+    # # header = {}
+    # # server.res_header(request, response, header)
+    # return res_content
+
 
 def form_data(request, response):
-    print(server.ROUTES)
     content = request["body"]
     content["status"] = "Done!! \n\nYour name has been submitted"
     res_content = json.dumps(content)
     return res_content
 
+
 def main():
     server.add_route("GET", "/form/<id>/user", render_form)
     server.add_route("POST", "/api/sendjson", form_data)
     server.execute_server()
+
 
 if __name__ == '__main__':
     main()
