ac9e2253121ab3186f61d1ee0ec8848f698fd25f
R3DDY97 <venkyreddy097@gmail.com>
2018-03-16 08:58:15 +0530

    updated web app

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-'''test application running to test HTTP server'''
+'''application running to test HTTP server'''
 
 import json
 import server
 
 def render_form(request, response, id):
-<<<<<<< HEAD
     print("$$$$$$$$$$$$", id)
     path = 'static/form.html'
-=======
-    path = './static/form.html'
->>>>>>> 70f71bb482444c3537726f08716a09b2f9fc5862
     with open(path, 'r') as file_obj:
         res_content = file_obj.read()
     server.res_status(response, 302)
@@ -22,7 +18,7 @@ def render_form(request, response, id):
 def form_data(request, response):
     print(server.ROUTES)
     content = request["body"]
-    content["status"] = "Done!! \nYour name has been submitted"
+    content["status"] = "Done!! \n\nYour name has been submitted"
     res_content = json.dumps(content)
     return res_content
 
