057a901f46937c09c0397dd5aa902bf1ab51c1b6
saumya-singh <saumya.singh0993@gmail.com>
2018-03-27 14:13:08 +0530

    styled the app acoording to PEP8

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""application running to test HTTP server"""
+"""Application running to test HTTP server."""
 
 import json
 import server
 
 
 def render_form(request, response, id):
-    """Setting HTML file to the response content."""
-    # return server.redirect(request, response, "https://www.geekskool.com/", \
-    #                                                                      307)
+    """Set HTML file to the response content."""
+    # return server.redirect(request, response, "https://www.geekskool.com/",
+    #                        307)
     path = 'static/form.html'
     with open(path, 'r') as file_obj:
         res_content = file_obj.read()
@@ -19,6 +19,7 @@ def render_form(request, response, id):
 
 
 def form_data(request, response):
+    """Send json data to the client."""
     content = request["body"]
     content["status"] = "Done!! \n\nYour name has been submitted"
     res_content = json.dumps(content)
@@ -26,6 +27,7 @@ def form_data(request, response):
 
 
 def main():
+    """Provide routes for the app."""
     server.add_route("GET", "/form/<id>/user", render_form)
     server.add_route("POST", "/api/sendjson", form_data)
     server.execute_server()
