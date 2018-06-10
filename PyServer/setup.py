d678e9d6187b999dfc8d7f730b9126141a77c1cf
saumya-singh1 <saumya.singh0993@gmail.com>
2018-06-10 18:50:24 +0530

    to make it a python package

---
+++ PyServer/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="PyWebServer",
+    version="0.0.1",
+    description="A simple HTTP web-server in Python3.5+ using asyncio module",
+    url="https://github.com/saumya-singh/web-server",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
